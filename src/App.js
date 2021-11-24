b82acbff3a53e898d870b16bb20a001d7737ff78
Umera <umeramalek796@gmail.com>
2021-11-24 00:56:25 -0800

    app.js(icons added)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,88 @@
 import React from 'react'
 import Header from './components/Header'
 import Project from './components/Project'
+import {Github,Linkedin, Medium} from './Icon';
 
-const App = () => {
+function App ()  {
+    
+    const Header = () =>
     return (
         <div>
             <Header/>
             <Project/>
         </div>
+    );
+
+
+const Icons = () => [
+        {
+            icon: <Github />,
+            link: 'https://github.com/umeramalek',
+        },
+        {
+            icon: <Linkedin />,
+            link: 'https://www.linkedin.com/in/umeramalek/',
+        },
+        {
+            icon: <Medium />,
+            link: 'https://medium.com/@umeramalek796',
+        },
+    ];
+    
+    return (
+        <div
+            style={{
+              width: '100vw',
+              height: '50px',
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              alignItems: 'center',
+              background: '#424242',
+              color: 'white',
+              position: 'fixed',
+              top: '0px',
+              zIndex: '666',
+            }}>
+    
+          </div>
     )
 }
+
+
+const Icons = () => [
+    {
+        icon: <Github />,
+        link: 'https://github.com/umeramalek',
+    },
+    {
+        icon: <Linkedin />,
+        link: 'https://www.linkedin.com/in/umeramalek/',
+    },
+    {
+        icon: <Medium />,
+        link: 'https://medium.com/@umeramalek796',
+    },
+];
+
+return (
+    <div
+        style={{
+          width: '100vw',
+          height: '50px',
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          background: '#424242',
+          color: 'white',
+          position: 'fixed',
+          top: '0px',
+          zIndex: '666',
+        }}>
+
+      </div>
+)
+
+
+
 
 export default App;
