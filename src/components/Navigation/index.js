eb76108b7ac737cf91c506922cc1470fc818ecfe
Umera <umeramalek796@gmail.com>
2021-11-25 03:15:21 -0800

    navbar finished

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
-import {MenuBar} from './MenuBar'
-import {Button} from '../Button'
-import "./style.css"
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+import './style.css'
 
 class Navbar extends Component {
-    state = {clicked:false }
+    state = { clicked: false }
 
     handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
+        this.setState({ clicked: !this.state.clicked })
     }
 
-    render(){
+    render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React <i className="fab fa-react"></i> </h1>
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
                 <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times': 'fas fa-bars'}></i>
-                
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
-                    {MenuBar.map((item,index) => { 
-                        return(
-                        <li key={index}> <a className ={item.cName} href={item.url}> {item.title} </a> </li>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
                         )
                     })}
                 </ul>
-                <Button>Home</Button>
             </nav>
         )
     }
 }
-
 
 export default Navbar
