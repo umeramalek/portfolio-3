5d6c393c6d293f1e5017f3781585cc4750842fd3
Umera <umeramalek796@gmail.com>
2021-11-25 02:39:44 -0800

    header styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 import {MenuBar} from './MenuBar'
+import {Button} from '../Button'
 import "./style.css"
 
-class Navigation extends Component {
+class Navbar extends Component {
+    state = {clicked:false }
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render(){
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React <i className="fab fa-react"></i> </h1>
-                <div className="menu-icon">
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times': 'fas fa-bars'}></i>
                 
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
                     {MenuBar.map((item,index) => { 
                         return(
                         <li key={index}> <a className ={item.cName} href={item.url}> {item.title} </a> </li>
                         )
                     })}
                 </ul>
+                <Button>Home</Button>
             </nav>
         )
     }
 }
 
 
-export default Navigation
+export default Navbar
