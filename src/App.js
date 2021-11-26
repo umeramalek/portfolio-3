5a8296decab4a2848732d5a2e9c0bb791cf3957f
Umera <umeramalek796@gmail.com>
2021-11-25 17:59:18 -0800

    added home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 import Navbar from './components/Navigation/index'
+import Home from './components/Home/Home'
 import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <Navbar />
-        </div>
-    )
+    const imageDetails = {
+        width: 524,
+        height: 650,
+      };
+      return (
+        <Router>
+          <Navbar/>
+          <Route
+            render={({ location }) => (
+            <AnimatePresence initial={false} exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                  <Route
+                    exact
+                    path='/'
+                    render={() => <Home imageDetails={imageDetails} />}
+                  />
+                </Switch>
+            </AnimatePresence>
+        )}
+      />
+    </Router>
+  );
 }
+
+
+//     return (
+//         <div className='App'>
+//             <Navbar />
+//             <Home />
+//         </div>
+//     )
+// }
 
 export default App
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,55 @@
-import React from 'react'
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import ProgressiveImage from "react-progressive-image";
 
-function Home() {
-    return (
-        <section className="company-heading intro-type" id="parallax-one">
-  <div class="container">
-    <div class="row product-title-info">
-      <div class="col-md-12">
-        <h1>DATA. DATA. DATA.</h1>
-        <a class="ct-btn-scroll ct-js-btn-scroll" href="#section2"><img alt="Arrow Down Icon" src="https://www.solodev.com/assets/anchor/arrow-down.png"></a>
-      </div>
-    </div>
-  </div>
-  <div class="parallax" id="parallax-cta" style="background-image:url(https://www.solodev.com/assets/anchor/company-hero2.jpg);"></div>
-</section>
-<div class="main">
-  <section id="section2">
-    <div class="container jumbo">
-      <div class="jumbotron">
-        <h2>Let Big Data Change the Game</h2>
-        <p>WebCorpCo is all about making sure your marketing stack is in alignment with your company as well as the customers you serve. There is no 'one size fits all' approach to marketing. Every business is unique, customers are unique, and your marketing should be as well.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-      </div>
-    </div>
-  </section>
-</div>
-    )
-}
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
-export default Home
+const Home = ({ imageDetails, image }) => (
+  <>
+    <main>
+      <div className='container'>
+        <div className='row center'>
+          <div className='image-container'>
+            <div
+              className='thumbnail'
+              ref={image}
+              style={{
+                width: imageDetails.width,
+                height: imageDetails.height,
+              }}>
+              <div className='frame'>
+                <Link to={`/model/yasmeen-tariq`}>
+                  <ProgressiveImage
+                    src={require("../images/yasmeen.webp")}
+                    placeholder={require("../images/compressed-image.jpg")}>
+                    {(src) => (
+                      <motion.img
+                        src={src}
+                        alt='Yasmeen Tariq'
+                        whileHover={{ scale: 1.1 }}
+                        transition={transition}
+                      />
+                    )}
+                  </ProgressiveImage>
+                </Link>
+              </div>
+            </div>
+            <motion.div
+              exit={{ opacity: 0 }}
+              transition={transition}
+              className='information'>
+              <div className='title'>Yasmeen Tariq</div>
+              <div className='location'>
+                <span>28.538336</span>
+                <span>-81.379234</span>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
+);
+
+export default Home;
