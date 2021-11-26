4499df87f3cbbda1a057f54298ce0508c02218b2
Umera <umeramalek796@gmail.com>
2021-11-25 22:35:10 -0800

    added updated about style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ export default function About() {
         <img src={me} alt='me' style={imageStyle}></img>
       </div>
       <p>
-      My name is Umera. Currently I am studying full stack web develpment at University of Berkeley. I am hoping to learn a lot and make use of the knowledge for my career path. Everyday I am learning new things and absolutely loving the challenge.
+      My name is Umera. Currently I am studying full stack web develpment at University of Berkeley. I am hoping to learn a lot and make use of the knowledge for my career path. The technical skills that I've gained from the coding bootcamp include HTML, Cascading Style Sheets (CSS), Javascript, jQuery, Node.js, Express, MySQL, Sequelize, Git, React, MongoDB, and REST APIs. Everyday I am learning new things and absolutely loving the challenge. 
       </p>
     </div>
   );
