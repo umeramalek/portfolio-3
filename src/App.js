29a41029b7cca5e44bfdfc0ed17e796e1faab8db
Umera <umeramalek796@gmail.com>
2021-11-25 19:57:22 -0800

    about section

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React from 'react';
-import { AnimatePresence } from "framer-motion";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
+import me from './images/me.jpg'
 import Navbar from './components/Navigation/index'
-import Home from './components/Home/Home'
 import './App.css';
 
+
 function App() {
-    const imageDetails = {
-        width: 524,
-        height: 650,
-      };
-      return (
-        <Router>
-          <Navbar/>
-          <Route
-            render={({ location }) => (
-            <AnimatePresence initial={false} exitBeforeEnter>
-                <Switch location={location} key={location.pathname}>
-                  <Route
-                    exact
-                    path='/'
-                    render={() => <Home imageDetails={imageDetails} />}
-                  />
-                </Switch>
-            </AnimatePresence>
-        )}
-      />
-    </Router>
-  );
+    return (
+        <div className='App'>
+            <Navbar />
+
+            <img src={me}  alt="my-image"/>
+  
+
+        </div>
+    )
 }
-
-
-//     return (
-//         <div className='App'>
-//             <Navbar />
-//             <Home />
-//         </div>
-//     )
-// }
 
 export default App
---
+++ src/components/About/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import me from '.../images/me.jpg';
+
+export default function About() {
+  const imageStyle = {
+    width: '70vw',
+    maxWidth: '400px',
+  };
+
+  return (
+    <div >
+      <div >
+        <img src={me} alt='me' style={imageStyle}></img>
+      </div>
+      <p>
+      My name is Umera. Currently I am studying full stack web develpment at University of Berkeley. I am hoping to learn a lot and make use of the knowledge for my career path. Everyday I am learning new things and absolutely loving the challenge.
+      </p>
+    </div>
+  );
+}
